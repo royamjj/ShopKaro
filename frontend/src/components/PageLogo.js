826a4dd1dc483f9348dc996e5701b4d2d5296064
rj00114 <jainroyamraj@.com>
2021-10-23 16:54:15 +0530

    Added PageLogo

---
+++ frontend/src/components/PageLogo.js
@@ -0,0 +1,10 @@
+import "../App.css";
+function PageLogo() {
+  return (
+    <div className="page-logo">
+      <div className="text">ShopKaro</div>
+    </div>
+  );
+}
+
+export default PageLogo;
--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -1,10 +1,12 @@
 import React from "react";
 import {Header} from '../components/Header';
 import {HomeProducts} from '../components/HomeProducts';
+import PageLogo from "../components/PageLogo";
 export function HomeScreen (){
     return (
         <React.Fragment>
             <Header/>
+            <PageLogo/>
             <HomeProducts/>
         </React.Fragment>
     );
