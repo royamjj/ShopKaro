363011fce5a9fdd9fa3b5be3d050f5a34c7ae6cf
rj00114 <jainroyamraj@.com>
2021-10-27 18:59:47 +0530

    added responsiveness to cart

--- frontend/src/components/CartItemComponent.js
+++ frontend/src/components/CartItemComponent.js
@@ -4,21 +4,20 @@ import './CartProductCss.css';
 import '../App.css';
 import { Link } from "react-router-dom";
 export default function CartItemComponent({props}){
-    console.log(props)
     return(
         <div className="cart-item-container">
-            <img className="cart-item-image" src={props.image} alt="item logo" />
-            
+            <div className="cart-item-image">
+            <img  src={props.image} alt="item logo" />
+            </div>
             <div className="cart-item-details">
                 <div className="cart-item-details-name">
-                <Link to={`/products/${props._id}`}>
-                <h2>{props.name}</h2>
+                    <Link to={`/products/${props._id}`}>
+                        <h2>{props.name}</h2>
                     </Link>
-                    
                 </div>
                 <div className="cart-item-details-all">
-                    <p>Quantity: {props.quantity}</p>
-                    <p>Total: Rs {props.total}</p>
+                    <p><span style={{color:"darkgreen"}}>Quantity: </span>{props.quantity}</p>
+                    <p><span style={{color:"green"}}>Total: </span>Rs {props.total}</p>
                     <p className="cart-item-delete"><i className="fa fa-trash" aria-hidden="true"></i></p>
                 </div>
             </div>
